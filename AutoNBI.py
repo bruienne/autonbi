e42c2c249fd1d250e4090554f66310b1a3a3ccf7
Erik Gomez <e@eriknicolasgomez.com>
2017-09-25 15:31:58 -0500

    remove beta app paths

--- AutoNBI.py
+++ AutoNBI.py
@@ -851,7 +851,7 @@ class processNBI(object):
             else:
                 print("Install source is 10.13 or newer, BaseSystem.dmg is in an alternate location...")
                 basesystemshadow = os.path.join(TMPDIR, 'BaseSystem.shadow')
-                basesystemdmg = os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/BaseSystem.dmg')
+                basesystemdmg = os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/BaseSystem.dmg')
 
             print("Running self.dmgresize...")
             result = self.runcmd(self.dmgresize(basesystemdmg, basesystemshadow, '8G'))
@@ -861,7 +861,7 @@ class processNBI(object):
             # print("Contents of plist:\n------\n%s\n------" % plist)
 
             basesystemplist = plistlib.readPlistFromString(plist)
-            
+
             # print("Contents of basesystemplist:\n------\n%s\n------" % basesystemplist)
 
             for entity in basesystemplist['system-entities']:
@@ -956,8 +956,8 @@ class processNBI(object):
 
             # High Sierra 10.13 contains the InstallESD.dmg as part of the installer app, remove it to free up space
             if isHighSierra:
-                if os.path.exists(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/InstallESD.dmg')):
-                    os.unlink(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/InstallESD.dmg'))
+                if os.path.exists(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/InstallESD.dmg')):
+                    os.unlink(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/InstallESD.dmg'))
 
         # Is Python or Ruby being added? If so, do the work.
         if addframeworks:
@@ -1055,17 +1055,17 @@ class processNBI(object):
             # For High Sierra, remove the chunklists for InstallESD and BaseSystem since they won't match
             # This includes removing chunklist entry from InstallInfo.plist
             if isHighSierra:
-                if os.path.exists(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/BaseSystem.chunklist')):
-                    os.unlink(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/BaseSystem.chunklist'))
-                if os.path.exists(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/InstallESD.chunklist')):
-                    os.unlink(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/InstallESD.chunklist'))
+                if os.path.exists(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/BaseSystem.chunklist')):
+                    os.unlink(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/BaseSystem.chunklist'))
+                if os.path.exists(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/InstallESD.chunklist')):
+                    os.unlink(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/InstallESD.chunklist'))
 
-                installinfoplist = plistlib.readPlist(os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/InstallInfo.plist'))
+                installinfoplist = plistlib.readPlist(os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/InstallInfo.plist'))
                 if installinfoplist['System Image Info'].get('chunklistid'):
                     del installinfoplist['System Image Info']['chunklistid']
                 if installinfoplist['System Image Info'].get('chunklistURL'):
                     del installinfoplist['System Image Info']['chunklistURL']
-                plistlib.writePlist(installinfoplist, os.path.join(nbimount, 'Install macOS High Sierra Beta.app/Contents/SharedSupport/InstallInfo.plist'))
+                plistlib.writePlist(installinfoplist, os.path.join(nbimount, 'Install macOS High Sierra.app/Contents/SharedSupport/InstallInfo.plist'))
 
         # We're done, unmount the outer NBI DMG.
         unmountdmg(nbimount)
